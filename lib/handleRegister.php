d1657b6fc0a7b28da1617f7bf3064e2156f0769d
Carlos Segarra <cs525@njit.edu>
2023-02-15 23:55:22 -0500

    quick fix for register function

--- lib/handleRegister.php
+++ lib/handleRegister.php
@@ -6,24 +6,22 @@ function handleRegister($username,$password){
         $db = getDB();
         $stmt = $db->prepare('INSERT INTO testusers (username, password) VALUES(:username, :pass)');
 
-            try{
-                $r = $stmt->execute([":username" => $username, ":password" => $password]);
-                
-                    echo "User registered: \n";
-                    return "valid";
-                        
+            try{ // maps username to username and password to password
+                $stmt->execute([":username" => $username, ":password" => $password]);
+                echo "User registered: $username";
+                return "valid";        
                 
             }
             catch (Exception $e){
                 //$e = json_decode($e,true);
-                preg_match("/testusers.(\w+)/",$e[2], $matches);
-                if(isset($matches[1]))
-                {
-                    echo $matches[1] . "is already in use!";
-                    return "";
-                }
-                else
-                {
+                // checks for error num for duplicate
+                if ($e[1] === 1062){ 
+                    preg_match("/testusers.(\w+)/", $e[2], $matches); 
+                    if (isset($matches[1])) { // if duplicate error look for username
+                    echo $matches[1]  . " is already in use!";
+                    return "duplicate";
+                    }
+                } else { // else we dont know what happened but maybe this'll give us a hint
                     echo error_log(var_export($e, true));
                     return "error";
                 }
