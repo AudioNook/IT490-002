dca265ce68df9c8c066ba648676d29b01e593d0e
Carlos Segarra <cs525@njit.edu>
2023-03-11 19:15:17 -0500

    Updated file structure to follow new hierarchy. All files have been moved accordingly.

---
+++ src/partials/nav.php
@@ -0,0 +1,50 @@
+<?php
+require_once(__DIR__ . "/../lib/functions.php");
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
+
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "$BASE_PATH",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}
+session_start();
+
+
+?>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Sample Web :)</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo get_url('index.php'); ?>">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo get_url('register.php'); ?>">Register</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ src/src_readme.md
@@ -0,0 +1,2 @@
+### SRC Dir
+This directory contains all of the application's source code. This includes all PHP files, JavaScript files, and CSS files, as well as any other code used by the application.
