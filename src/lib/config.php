e20a08548ec6f1e706757203ca6ab361fab4119d
carlomos <carlomos@it490.carlomos.virtualbox.org>
2023-03-11 21:50:12 -0500

    Adding new config

---
+++ src/lib/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// DB Credentials
+$dbhost = '127.0.0.1';
+$dbuser = 'testuser';
+$dbpass = '12345';
+$dbdatabase = 'testdb';
+
+// Rabbit MQ Configs
+
+$rabbit_ini = "rabbitMQ.ini";
+$rabbit_server = "testServer";
+
+
+echo "rabbit_ini = $rabbit_ini";
+echo "rabbit_server = $rabbit_server";
+
+?>
