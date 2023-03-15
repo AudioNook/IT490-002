7c5474a51e2aa235a9dde5fcdd7cea735ba13dc0
Carlomos7 <cs525@njit.edu>
2023-03-15 17:36:22 -0400

    Added logout logic

---
+++ src/lib/jwt_handling/delete_jwt.php
@@ -0,0 +1,30 @@
+<?php
+//header('Content-Type: application/json');
+require_once(__DIR__ . "/../functions.php");
+require_once(__DIR__ . "/../../../vendor/autoload.php");
+require_once(__DIR__ . "/../config.php");
+
+function delete_jwt() {
+    // Get JWT from cookie
+    $jwt = $_COOKIE["jwt"];
+    global $rbMQc;
+    $logout_req = array();
+    $logout_req['type'] = 'logout';
+    $logout_req['token'] = $jwt;
+    $response = json_decode($rbMQc->send_request($logout_req), true);
+    switch($response['code']){
+        case 200:
+            // Remove JWT cookie
+            unset($_COOKIE["jwt"]);
+            setcookie("jwt", "", -1, "/");
+            // Redirect to login page
+            redirect("login.php");
+            break;
+        case 500:
+            error_log($response['message']);
+            break;
+        default:
+            echo "idk what happened bro";
+    }
+}
+?>
--- src/lib/jwt_handling/jwt_functions.php
+++ src/lib/jwt_handling/jwt_functions.php
@@ -1,5 +1,5 @@
 <?php
-/*require(__DIR__ . "/generate_jwt.php");
+require(__DIR__ . "/generate_jwt.php");
 require(__DIR__ . "/validate_jwt.php");
 require(__DIR__ . "/delete_jwt.php");
-require(__DIR__ . "/check_jwt.php");*/
+require(__DIR__ . "/check_jwt.php");
