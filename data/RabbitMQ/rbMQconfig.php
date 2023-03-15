b38db3ffc0d42fc7b822b73f96678e1642b2c0e5
Carlomos7 <cs525@njit.edu>
2023-03-15 03:08:51 -0400

    created new rbMQconfig.php for configs

---
+++ data/RabbitMQ/rbMQconfig.php
@@ -0,0 +1,7 @@
+<?php
+$rabbit_ini = "rabbitMQ.ini";
+$rabbit_server = "testServer";
+
+require(__DIR__ . "/path.inc");
+require(__DIR__ . "/get_host_info.inc");
+require_once(__DIR__ . "/rabbitMQLib.inc");
