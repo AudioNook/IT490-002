6c669da20c0696778a278e9fad1de238e54be1ae
Carlomos7 <cs525@njit.edu>
2023-03-15 03:39:07 -0400

    Updated return statements to be more concise and readable

--- src/lib/handleLogin.php
+++ src/lib/handleLogin.php
@@ -12,12 +12,12 @@ function handleLogin($username,$password){
                     if($user){
                         $pass = $user["password"];
                         if($pass == $password){
-                            $jwt = generate_jwt($db,$user);
+                            //$jwt = generate_jwt($db,$user);
                             return [
                                 'code' => 200,
                                 'status' => 'success',
                                 'message' => 'Valid login credentials.',
-                                'token' => $jwt['token'],
+                                /*'token' => $jwt['token'],*/
                                 'expiry' => $jwt['expiry']
                                     ];
                         } else {
