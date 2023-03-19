722ec7f2fcc29d33f4f9c58c0b3fce0dc2bb21ae
jmpearson135 <jmpearson135@hotmail.com>
2023-03-19 00:59:53 -0400

    Improved the design of the search page added view details and filber dropdown (filter dropdown not yet functional)

--- src/api/index.php
+++ src/api/index.php
@@ -12,6 +12,7 @@ if (isset($_POST['submit'])) {
     $release_title = '';
     $type = 'release';
     $format = 'vinyl';
+    
     $per_page = 96;
     $page = 1;
     $url = "https://api.discogs.com/database/search?" .
@@ -47,36 +48,75 @@ if (isset($_POST['submit'])) {
 
     // Close the cURL session
     curl_close($ch);
+
     
+    //loops through the results and creates a $allGenres array to populated the filter dropdown.
+    $allGenres = array();
+    foreach ($results as $res)
+    {
+    foreach($res['genre'] as $gen) 
+    { 
+    if(!in_array($gen, $allGenres))
+    {
+      array_push($allGenres, $gen);
+    }
+  }
 }
+}
+    
+
+    
 
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <title>Discogs Search</title>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>AudioNook Search</title>
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Discogs Artist Search</h1>
+            <h1>AudioNook Search</h1>
             <form method="POST" action="">
                 <div class="mb-3">
-                    <label for="artist" class="form-label">Artist</label>
-                    <input type="text" class="form-control" id="artist" name="artist" placeholder="Enter artist name" required>
+                    
+                    <input type="text" class="form-control" id="artist" name="artist" placeholder="Search..." required>  <button type="submit" name="submit" class="btn btn-info">Search</button>
                 </div>
-                <button type="submit" name="submit" class="btn btn-info">Search</button>
+               
                 <p id = "contact-btn" style = "float:right;"> <a href="#" class="btn btn-primary" onClick="toggleFields()">Details View</a> </p>
+                <div class="container">
+                    <div class="btn-group">
+                        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            data-bs-auto-close="true" aria-expanded="false">
+                            Filter
+                        </button>
+                        <ul class="dropdown-menu">
+                          <?php foreach($allGenres as $g)/*Populates the filter dropdown*/:?>
+                            
+                            <li><a class="dropdown-item" href="#"><?php echo($g);?></a></li>
+                          <?php endforeach ?>  
+                        </ul>
+                    </div>
+                </div>
             </form>
         </div>
 </div>
 <script>
+   $(function() {
+                $('#details1, #details2')
+                    .hide();
+            });
   function toggleFields(){
-    $('#details1, #details2').toggle();
+    $('#details1, #details2').toggle("slow");
     }
 </script>
 <?php if (isset($results) && !empty($results)):?>
@@ -87,7 +127,7 @@ if (isset($_POST['submit'])) {
           <img class="card-img-top" src="<?php echo $result['cover_image']   ?>" alt="Album Covers" onerror="this.onerror=null;this.src='https://tinyurl.com/5n7fs4w8';" width = "300" height = "300" > 
             <div class="card-body" style = "max-height = 350px ">
               <h5 class="card-title" ><?php echo htmlspecialchars($result['title']?? "n/a"); ?></h5>
-              <p id= "details1" class="card-text"><?php echo htmlspecialchars($result['year'] . " " ?? "n/a"); echo htmlspecialchars($result['country'] ?? "n/a"); echo '<br> <br>'; echo "Genre: <br>"; foreach($result['genre'] as $genre) {echo $genre, ' ';}   ?></p>
+              <p id= "details1" class="card-text"><?php echo htmlspecialchars($result['year'] . " " ?? "n/a"); echo htmlspecialchars($result['country'] ?? "n/a"); echo '<br> <br>'; echo "Genre: <br>";  foreach($result['genre'] as $genre) {echo $genre, ' '; } ?></p>
               <p class="card-text" id= "details2"><?php echo "Formats: <br>"; foreach($result['format'] as $formats) {echo $formats, '  ';} echo '<br><br>';  ?> </p>
               <p style = " padding: 5px;"> <a href="#" class="btn btn-success" >Add to Collection</a> </p>
               
@@ -103,3 +143,4 @@ if (isset($_POST['submit'])) {
 </html>
 <?php
 require(__DIR__ . "/pagination.php");
+?>
