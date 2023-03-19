cd3edaa4ee3accb83379a7d62d9d7a69731354e5
Jaylin Cabrera <jc982@njit.edu>
2023-03-19 13:53:59 -0400

    made changes to profile page, added creds

--- src/public/profile.php
+++ src/public/profile.php
@@ -12,10 +12,44 @@ logged_in(true);
 <script>
   //validateJWT();
 </script>
-    <title>Profile</title>
+    <title>My Profile</title>
 </head>
 <body>
-    <h1>Welcome to your Profile!</h1>
-    <p>Here you can view and edit your account information.</p>
+<section style="background-color: #eee;">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col">
+        <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+        <h5 class="my-3">My Profile</h5>
+        </nav>
+      </div>
+    </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Username</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">$username</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">$email</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
