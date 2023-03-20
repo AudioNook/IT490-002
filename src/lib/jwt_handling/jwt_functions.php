7c11dd3a1ddd42c3020f787cbf9ada9cd67a5210
Carlos Ray <106279547+Carlomos7@users.noreply.github.com>
2023-03-19 22:19:44 -0400

    Update jwt_functions.php

--- src/lib/jwt_handling/jwt_functions.php
+++ src/lib/jwt_handling/jwt_functions.php
@@ -4,11 +4,14 @@ require_once(__DIR__ . "/../../../vendor/autoload.php");
 require_once(__DIR__ . "/../config.php");
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+
 function generate_jwt($user){
     // Revoke older token if one exists
+    $db = getDB();
     $table_name= 'JWT_Sessions';
     $delete_query = "DELETE FROM $table_name WHERE user_id = :user_id AND expires_at > NOW()";
-    executeQuery($delete_query, [":user_id" => $user['id']]);
+    $stmt = $db->prepare($delete_query);
+    $stmt->execute([":user_id" => $user['id']]);
 
     // Password matches, generate JWT token
     $issuedAt = new DateTimeImmutable();
@@ -23,56 +26,59 @@ function generate_jwt($user){
 
     // Store the JWT session in the database
     $insert_query = "INSERT INTO $table_name (user_id, token, expires_at, issued_at) VALUES (:user_id, :token, :expires_at, :issued_at)";
-    $params = [
-        ':user_id' => $user['id'],
-        ':token' => $token,
-        ':expires_at' => $expiry->format('Y-m-d H:i:s'),
-        ':issued_at' => $issuedAt->format('Y-m-d H:i:s'),
-    ];
-
-    try {
-        if (executeQuery($insert_query, $params)) {
-            return array(
-                'token' => $token,
-                'expiry' => $expiry->getTimestamp()
-            );
-        } else {
-            throw new Exception('Error saving JWT session: Failed to execute query');
-        }
-    } catch (Exception $e) {
+    $stmt = $db->prepare($insert_query);
+    try{
+        $stmt->execute([
+            ':user_id' => $user['id'],
+            ':token' => $token,
+            ':expires_at' => $expiry->format('Y-m-d H:i:s'),
+            ':issued_at' => $issuedAt->format('Y-m-d H:i:s'),
+        ]);
+        return array(
+            'token' => $token,
+            'expiry' => $expiry->getTimestamp()
+        );
+    }
+    catch (Exception $e){
         throw new Exception('Error saving JWT session: ' . $e->getMessage());
     }
 }
-function validate_jwt($jwt)
-{
+function validate_jwt($jwt) {
     $key = JWT_SECRET;
+    $db = getDB();
     $table_name = "JWT_Sessions";
     $query = "SELECT * FROM $table_name WHERE token = :token AND user_id = :user_id AND expires_at > NOW()";
+    $stmt = $db->prepare($query);
+    try {
+        $decoded = JWT::decode($jwt, new Key($key, 'HS256'));           
+        // Look up the JWT in the database
+        $stmt->execute([":token" => $jwt, ":user_id" => $decoded->userid]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            // Check if the token is expired
+            $expiry = strtotime((string) $row['expires_at']);
+            if (time() < $expiry) {
 
-    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
-    // Look up the JWT in the database
-    $params = [":token" => $jwt, ":user_id" => $decoded->userid];
-    $rows = executeQuery($query, $params);
-    if ($rows && count($rows) > 0) {
-        $row = $rows[0];
-        // Check if the token is expired
-        $expiry = strtotime((string) $row['expires_at']);
-        if (time() < $expiry) {
-
-            return [
-                'type' => 'validate_jwt',
-                'code' => 200,
-                'status' => 'success',
-                'message' => 'Valid token.',
-            ];
+                return [
+                    'code' => 200,
+                    'status' => 'success',
+                    'message' => 'Valid token.',
+                        ];
+            }
         }
+        return [
+            'code' => 401,
+            'status' => 'error',
+            'message' => 'invalid token.',
+                ];
+    } catch (Exception $e) {
+        $error_message = var_export($e, true);
+        return [
+            'code' => 500,
+            'status' => 'error',
+            'message' => $error_message,
+        ];
     }
-    return [
-        'type' => 'validate_jwt',
-        'code' => 401,
-        'status' => 'error',
-        'message' => 'invalid token.',
-    ];
 }
 function delete_jwt() {
     // Get JWT from cookie
@@ -82,50 +88,46 @@ function delete_jwt() {
     $logout_req['type'] = 'logout';
     $logout_req['token'] = $jwt;
     $response = json_decode($rbMQc->send_request($logout_req), true);
-    if($response['type'] == 'logut'){
-        switch($response['code']){
-            case 200:
-                // Remove JWT cookie
-                unset($_COOKIE["jwt"]);
-                setcookie("jwt", "", -1, "/");
-                // Redirect to login page
-                redirect("login.php");
-                break;
-            case 500:
-                error_log($response['message']);
-                break;
-            default:
-                echo "idk what happened bro";
-        }
+    switch($response['code']){
+        case 200:
+            // Remove JWT cookie
+            unset($_COOKIE["jwt"]);
+            setcookie("jwt", "", -1, "/");
+            // Redirect to login page
+            redirect("login.php");
+            break;
+        case 500:
+            error_log($response['message']);
+            break;
+        default:
+            echo "idk what happened bro";
     }
 }
-function check_jwt($rbMQc)
-{
+function check_jwt(){
     if (isset($_COOKIE["jwt"]) && !empty($_COOKIE["jwt"])) {
         $jwt = $_COOKIE["jwt"];
+        global $rbMQc;
         $jwt_req = array();
         $jwt_req['type'] = 'validate_jwt';
         $jwt_req['token'] = $jwt;
         $response = json_decode($rbMQc->send_request($jwt_req), true);
-        if ($response['type'] == 'validate_jwt') {
-            switch ($response['code']) {
-                case 200:
-                    error_log("check_jwt: Good little cookie");
-                    break;
-                case 401:
-                    // Remove JWT cookie
-                    unset($_COOKIE["jwt"]);
-                    setcookie("jwt", "", -1, "/");
-                    // Session no longer valid please log back in
-                    // Redirect to login page
-                    redirect("login.php");
-                    break;
-                default:
-                    unset($_COOKIE["jwt"]);
-                    setcookie("jwt", "", -1, "/");
-                    error_log($response['message']);
-                    redirect("login.php");
-            }
-        }
+        switch($response['code']){
+        case 200:
+            error_log("check_jwt: Good little cookie");
+            break;
+        case 401:
+            // Remove JWT cookie
+            unset($_COOKIE["jwt"]);
+            setcookie("jwt", "", -1, "/");
+            // Session no longer valid please log back in
+            // Redirect to login page
+            redirect("login.php");
+            break;
+        default:
+            unset($_COOKIE["jwt"]);
+            setcookie("jwt", "", -1, "/");
+            error_log($response['message']);
+            redirect("login.php");
+       }
     }
 }
