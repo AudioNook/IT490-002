f89b07bd8f43731d194760050fae2735418b9dd6
Jaylin Cabrera <jc982@njit.edu>
2023-03-20 23:22:47 -0400

    formatting fix

--- src/public/review.php
+++ src/public/review.php
@@ -1,67 +1,60 @@
 <?php require(__DIR__ . "/../partials/nav.php"); 
-logged_in(true);
-
-$reviews_req = array();
-$reviews_req['type'] = 'reviews';
-$reviews_req['message'] = "Sending discussion reviews request";
-$reviews;
-$response = json_decode($rbMQc->send_request($reviews_req), true);
-
-if ($response['type'] == 'reviews') {
-  switch ($response['code']) {
-    case 200:
-      $reviews = $response['reviews'];
-      break;
-    case 401:
-      echo '<script language="javascript">';
-      echo 'alert("' . $response['message'] . '")';
-      echo '</script>';
-      break;
-    default:
-      echo ($response['message']);
-  }
-}
-?>
-
-
-
+   logged_in(true);
+   
+   $reviews_req = array();
+   $reviews_req['type'] = 'reviews';
+   $reviews_req['message'] = "Sending discussion reviews request";
+   $reviews;
+   $response = json_decode($rbMQc->send_request($reviews_req), true);
+   
+   if ($response['type'] == 'reviews') {
+     switch ($response['code']) {
+       case 200:
+         $reviews = $response['reviews'];
+         break;
+       case 401:
+         echo '<script language="javascript">';
+         echo 'alert("' . $response['message'] . '")';
+         echo '</script>';
+         break;
+       default:
+         echo ($response['message']);
+     }
+   }
+   ?>
 <html>
-
-<head>
-  <script>
-    //validateJWT();
-  </script>
-  <title>Reviews</title>
-</head>
-<div class="container-fluid">
-
-</div>
-<div class="container-fluid">
-  <?php if (!empty($reviews)) : ?>
-    <?php foreach ($reviews as $review) : ?>
+   <head>
+      <script>
+         //validateJWT();
+      </script>
+      <title>Reviews</title>
+   </head>
+   <div class="container-fluid">
+   </div>
+   <div class="container-fluid">
+      <?php if (!empty($reviews)) : ?>
+      <?php foreach ($reviews as $review) : ?>
       <div class="container-fluid">
-  <div>
-    <div class="card m-3">
-      <div class="card-header d-flex justify-content-between">
-        <div>Product ID: <?php echo $review['product_id']; ?></div>
-        <div>Product Name: <?php echo $review['product_name']; ?></div>
+         <div>
+            <div class="card m-3">
+               <div class="card-header d-flex justify-content-between">
+                  <div>Product ID: <?php echo $review['product_id']; ?></div>
+                  <div>Product Name: <?php echo $review['product_name']; ?></div>
+               </div>
+               <div class="card-body">
+                  <p class="card-text">Comment: <?php echo $review['comment']; ?></p>
+               </div>
+               <div class="card-body">
+                  <p class="card-text">Created: <?php echo $review['created']; ?></p>
+               </div>
+            </div>
+         </div>
       </div>
-      <div class="card-body">
-        <p class="card-text">Comment: <?php echo $review['comment']; ?></p>
-      </div>
-      <div class="card-body">
-        <p class="card-text">Created: <?php echo $review['created']; ?></p>
-      </div>
-    </div>
-  </div>
-</div>
-    <?php endforeach; ?>
-  <?php else : ?>
-  <?php endif; ?>
-</div> 
+      <?php endforeach; ?>
+      <?php else : ?>
+      <?php endif; ?>
+   </div>
 </html>
-
-
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -72,11 +65,11 @@ if ($response['type'] == 'reviews') {
       <div class="container">
          <h1>Review a Product!</h1>
          <form>
-            <label for="search">Product Search:</label>
-            <input type="text" id="search" name="search">
-            <input type="submit" value="Search">
-         </form>
-         <form>
+         <form action="/action_page.php">
+            <label for="fname">Product ID:</label><br>
+            <input type="text" id="fname" name="fname" value=""><br>
+            <label for="lname">Product Name:</label><br>
+            <input type="text" id="lname" name="lname" value=""><br><br>
             <label for="review">Write a review:</label>
             <input type="text" id="review" name="review" style="height: 150px;">
             <input type="submit" value="Review">
