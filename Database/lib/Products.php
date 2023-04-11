46628884a407d520b4581e16ab72355d20d7d60a
Jaylin Cabrera <jc982@njit.edu>
2023-04-11 17:54:33 -0400

    separated reviews from products file

--- Database/lib/Products.php
+++ Database/lib/Products.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . "/db.php");
 
 use Database\db;
 /**
- * Classwa Products and Reviews: Database class for Products and Reviews
+ * Class Products: Database class for Products
  * @package Database
  */
 
@@ -31,70 +31,21 @@ class Products extends db
     {
         $table = 'Products';
         $products = $this->exec_query("SELECT id, name, category, stock, cost, image FROM $table");
-        if ($products) {
-            return $products;
+        if ($products !== false && !empty($products)){
+            return [
+                'code'=>200,
+                'message'=> 'Sending product data',
+                'products' => $products
+            ];
+        }
+        else{
+            return [
+                'code'=>400,
+                'message'=> 'Unable to retrieve product data',
+            ];
         }
     }
 }
 
-///////////////////////////////
-//Reviews Class
-///////////////////////////////
-
-class Reviews extends db
-{
-    /**
-     * Forums constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-    /**
-     * Get all reviews in Review Table
-     * @return array
-     */
-    public function get_reviews()
-    {
-        $table = 'Reviews';
-        $reviews = $this->exec_query("SELECT id, product_id, comment, created FROM $table");
-        if ($reviews) {
-            return $reviews;
-        }
-    }
-
-    /**
-     * Get all reviews for a specific user
-     * @param $product_id
-     * @return array
-     */
-    public function get_user_reviews($product_id)
-    {
-        $reviews = $this->exec_query("SELECT Reviews.id, Reviews.product_id, Reviews.comment, Reviews.created FROM Reviews INNER JOIN Users ON Reviews.user_id = Users.id WHERE Reviews.product_id = :p_id", [":p_id" => $product_id]);
-        if ($reviews) {
-            return $reviews;
-        }
-    }
-    /**
-     * Get a single review
-     * @param $id
-     * @return array
-     */
-    public function create_review($id, $user_id, $product_id, $comment, $created)
-    {
-        $query = "INSERT INTO Reviews (id, user_id, product_id, comment, created) VALUES(:id, :user_id. :product_id, :comment, :created)";
-        $params = [
-            ':id' => $id,
-            ':user_id' => $user_id,
-            ':product_id' => $product_id,
-            ':comment' => $comment,
-            ':created' => $created
-
-        ];
-        if ($this->exec_query($query, $params) !== false) {
-            return true;
-        }
-    }
-}
 
 
