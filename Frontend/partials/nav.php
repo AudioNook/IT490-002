eab51163c0b43b75e747feeda3b6d2c36b6154d4
Carlos Ray <cs525@njit.edu>
2023-04-15 18:44:25 -0400

    AudioNook Logo now Redirects to Marketplace

--- Frontend/partials/nav.php
+++ Frontend/partials/nav.php
@@ -33,7 +33,7 @@ if (basename($_SERVER['PHP_SELF']) !== 'login.php') { // check if the current pa
             </li>
          <?php endif; ?>
          <?php if ($is_logged_in) : ?>
-            <a class="navbar-brand" href="#">AudioNook</a>
+            <a class="navbar-brand" href="<?php echo get_url('marketplace.php'); ?>">AudioNook</a>
             <li class="nav-item">
                <a class="nav-link" href="<?php echo get_url('start_collecting.php'); ?>">Add to Collection</a>
             </li>
