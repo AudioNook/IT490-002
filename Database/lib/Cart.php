a49db15db043bfde2a2f3b68010d3a002e01afd7
Carlos Ray <cs525@njit.edu>
2023-04-16 18:36:26 -0400

    Added handle the case where there's no cart data for a specific user_id

--- Database/lib/Cart.php
+++ Database/lib/Cart.php
@@ -21,13 +21,13 @@ class Cart extends db
     }
     //cart actions
     public function cart($request)
-    {       
+    {
         if (array_key_exists('action', $request)) {
-            $action = strtolower($request['action']);
+            $action = $request['action'];
             switch ($action) {
                 case "add": // add item to cart
                     $query = "INSERT INTO Cart (product_id, unit_price, user_id)
-                    VALUES (:iid, (SELECT cost FROM Products where id = :iid), :uid)";
+                    VALUES (:iid, (SELECT price FROM marketplace_items where id = :iid), :uid)";
                     $stmt = $this->prepare($query);
                     $stmt->bindValue(":iid", $request['product_id'], PDO::PARAM_INT);
                     $stmt->bindValue(":uid", $request['user_id'], PDO::PARAM_INT);
@@ -50,9 +50,11 @@ class Cart extends db
                     }
                     break;
                 case "update": // update item in cart
-                    $query = "UPDATE Cart WHERE id = :cid AND user_id = :uid";
+                    $query = "UPDATE Cart set desired_quantity = :dq WHERE id = :cid AND user_id = :uid";
                     $stmt = $this->prepare($query);
-                    //cart id specifies a specific cart item
+                    //desired quantity is the new quantity
+                    $stmt->bindValue(":dq", $request['desired_quantity'], PDO::PARAM_INT);
+                    //cart id specifies a specific cart
                     $stmt->bindValue(":cid", $request['cart_id'], PDO::PARAM_INT);
                     //user id ensures we can only edit our cart
                     $stmt->bindValue(":uid", $request['user_id'], PDO::PARAM_INT);
@@ -65,6 +67,38 @@ class Cart extends db
                             'message' => 'Sucessfully updated item in cart',
                         ];
                     } catch (PDOException $e) {
+                        if ($request['desired_quantity'] < 0) {
+                            error_log(var_export($e, true));
+                            return [
+                                'type' => 'req_cart',
+                                'code' => 401,
+                                'status' => 'error',
+                                'message' => 'Error updating item in cart: Desired quantity cannot be less than 0',
+                            ];
+                        }
+                        if ($request['desired_quantity'] == 0) {
+                            $query = "DELETE FROM Cart WHERE id = :cid AND user_id = :uid";
+                            $stmt = $this->prepare($query);
+                            $stmt->bindValue(":cid", $request["cart_id"], PDO::PARAM_INT);
+                            $stmt->bindValue(":uid", $request['user_id'], PDO::PARAM_INT);
+                            try {
+                                $stmt->execute();
+                                return [
+                                    'type' => 'req_cart',
+                                    'code' => 200,
+                                    'status' => 'Success',
+                                    'message' => 'Sucessfully deleted item from cart',
+                                ];
+                            } catch (PDOException $e) {
+                                error_log(var_export($e, true));
+                                return [
+                                    'type' => 'req_cart',
+                                    'code' => 401,
+                                    'status' => 'Error',
+                                    'message' => 'Error deleting item in cart',
+                                ];
+                            }
+                        }
                         return [
                             'type' => 'req_cart',
                             'code' => 401,
@@ -148,6 +182,14 @@ class Cart extends db
                     'status' => 'Success',
                     'message' => 'Created cart',
                     'cart' => $cart
+                ];
+            } else {
+                return [
+                    'type' => 'req_cart',
+                    'code' => 200,
+                    'status' => 'Success',
+                    'message' => 'Cart is empty',
+                    'cart' => []
                 ];
             }
         } catch (PDOException $e) {
