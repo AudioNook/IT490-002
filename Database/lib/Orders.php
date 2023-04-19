0b89eeb80eb9d65a2dea5731273afc668760d888
Carlos Segarra <cs525@njit.edu>
2023-04-19 17:45:11 -0400

    checkout LOGIC

---
+++ Database/lib/Orders.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database;
+
+require_once(__DIR__ . "/db.php");
+
+use Database\db;
+
+class Orders extends db{
+    
+} 
--- Frontend/lib/utils/util_functions.php
+++ Frontend/lib/utils/util_functions.php
@@ -4,3 +4,4 @@ require(__DIR__ . "/redirect.php");
 require(__DIR__ . "/paginate.php");
 require(__DIR__ . "/rabbitmq_client.php");
 require(__DIR__ . "/review_stars.php");
+require(__DIR__ . "/get_states_arr.php");
