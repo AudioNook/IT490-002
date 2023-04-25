4a1e35edded64019df594659dbc74fc4d85e9808
Jaylin Cabrera <jc982@njit.edu>
2023-04-25 18:24:28 -0400

    added version and packages tables + curl rename

---
+++ Database/sql/020_CREATE_PACKAGES_TABLE.sql
@@ -0,0 +1,9 @@
+/* stores our packages information */
+CREATE TABLE IF NOT EXISTS Packages(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    version_id INT NOT NULL,
+    environment ENUM('dev', 'qa', 'prod') NOT NULL,
+    package_type ENUM('fe', 'be', 'db', 'dmz') NOT NULL,
+    package_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (version_id) REFERENCES Versions(id)
+);
