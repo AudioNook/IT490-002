acc22caf6c4509257875568e789ce0bf138df8d1
Carlos Ray <cs525@njit.edu>
2023-04-25 21:31:21 -0400

    switched back to scp, sftp not working properly

--- Deploy/lib/NiceSSH.php
+++ Deploy/lib/NiceSSH.php
@@ -20,8 +20,9 @@ class NiceSSh{
         // ssh2_auth_password() - authenticate over SSH using a plain password
         // https://www.php.net/manual/en/function.ssh2-auth-password.php
         if(!ssh2_auth_password($session, $user, $pass)){
-            echo 'Authentication failed for ' . $user . '@' . $host . "\n";
+            die('Authentication failed for ' . $user . '@' . $host . "\n");
         }
+        echo "Connected! \n";
         return $session;
     }
     /**
@@ -32,7 +33,13 @@ class NiceSSh{
     public function exec_command($session,$command){
         // executes a command on a remote server
         // https://www.php.net/manual/en/function.ssh2-exec.php
-        ssh2_exec($session, $command);
+        echo "executing: " . $command . "\n";
+        $stream = ssh2_exec($session, $command);
+        // Idk how this works it just does: https://www.php.net/manual/en/function.stream-set-blocking.php
+        stream_set_blocking($stream, true);
+        $output = stream_get_contents($stream);
+        fclose($stream);
+        echo $output;
     }
     /**
      * Execute multiple commands on a remote server
@@ -43,7 +50,7 @@ class NiceSSh{
         // executes multiple commands on a remote server
         // https://www.php.net/manual/en/function.ssh2-exec.php
         foreach($commands as $command){
-            ssh2_exec($session, $command);
+            $this->exec_command($session, $command);
         }
     }
 
@@ -57,7 +64,7 @@ class NiceSSh{
         // retrieves a file from the remote server
         // https://www.php.net/manual/en/function.ssh2-scp-recv.php
         if(!ssh2_scp_recv($session, $remote_file, $local_file)){
-            echo 'Failed to retrieve remote file ' . $remote_file . "\n";
+            die('Failed to retrieve remote file ' . $remote_file . "\n");
         }
     }
 
@@ -69,9 +76,37 @@ class NiceSSh{
      */
     public function send_file($session, $local_file, $remote_file){
         // sends a file from the local server to the remote server
-        // https://www.php.net/manual/en/function.ssh2-scp-send.php
+        // Referenced: https://www.php.net/manual/en/function.ssh2-sftp.php
+        // Referenced: https://www.codexpedia.com/php/php-ssh2-upload-and-download-files-through-sftp/
+        // Referenced: https://www.php.net/manual/en/function.ssh2-scp-send.php
+        echo "Attempting to send: " . $local_file . "\n";
         if(!ssh2_scp_send($session, $local_file, $remote_file)){
-            echo 'Failed to send file ' . $local_file . ' to ' . $remote_file . "\n";
+            die('Failed to send local file ' . $local_file . "\n");
         }
+        echo "Sent: " . $local_file . "\n";
+    }
+
+    public function remove_file($session, $file){
+        // Removes contents of specified remote path
+        // https://www.php.net/manual/en/function.ssh2-sftp-unlink.php
+        $sftp = ssh2_sftp($session);
+        ssh2_sftp_unlink($sftp, $file);
+    }
+
+    public function remove_dir($session, $dir){
+        // removes a directory from the remote server
+        // https://www.php.net/manual/en/function.ssh2-sftp-rmdir.php
+       // Changed the to use exec_command() instead of ssh2_sftp_rmdir() because it doesn't work on non-empty directories
+        /* $sftp = ssh2_sftp($session);
+        rmdir("ssh2.sftp://$sftp$dir");*/
+        echo "removing $dir";
+        $command = "rm -rf {$dir}*";
+        ssh2_exec($session, $command);
+    }
+
+    public function set_chmod_permissions($session, $path, $permissions)
+    {
+        $command = "chmod {$permissions} {$path}";
+        $this->exec_command($session, $command);
     }
 }
