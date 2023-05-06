2fc38bf30bb5eac23ec828cec77307e6697c8b9b
jmpearson135 <jmpearson135@hotmail.com>
2023-05-06 19:17:20 -0400

    Added window where QR code is shown so the user can add it to their google authenticator app and once they add it they can click the button and be redirected to login.

--- Frontend/public/register.php
+++ Frontend/public/register.php
@@ -1,5 +1,6 @@
 <?php
 require(__DIR__ . "/../partials/nav.php");
+$qrCode = null;
 ?>
 <div id="alert_msg"></div>
 <div class="container-fluid">
@@ -22,10 +23,23 @@ require(__DIR__ . "/../partials/nav.php");
             <input class="form-control" type="password" placeholder="P@ssword1" id="confirm" name="confirm"/>
 
         </div>
-        <input type="submit" name="submit" class="mt-3 btn btn-primary" value="register" />
+        <input type="submit" name="submit" class="mt-3 btn btn-primary" value="register"/>
     </form>
 </div>
-
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content shadow p-3 mb-5 border-0" style="width: 18rem;">
+    <img class="card-img-top" src="qrcode.svg" alt="QR Code">
+      <div class="modal-body">
+      <h1 class="heading"><strong>Scan QR Code</strong></h1>
+      <p class="card-text">Scan the QR code set up Two Factor Authentication</p>
+      </div>
+      <div class="modal-footer">
+        <a type="button" href="<?php echo get_url('profile.php'); ?>" class="btn btn-primary">Scanned</a>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <?php
@@ -38,7 +52,7 @@ if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["passwor
     $password = $_POST['password'];
     $confirm = $_POST['confirm'];
     $patternName = '/^[a-z0-9_-]{3,16}$/';
-    $patternPassword = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
+    $patternPassword = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/';
 
     // sanitize email here
     
@@ -80,7 +94,12 @@ if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["passwor
     //If there are no validation errors
     if(!$hasError){
         $request = new DBRequests();
-        $request->register($username, $email, $password);
+        $qrCodeUrl = $request->register($username, $email, $password);
+        echo '<script>';
+        echo 'var otpModal = new bootstrap.Modal(document.getElementById("staticBackdrop"));';
+        echo 'otpModal.show();';
+        echo '</script>';
+        //do bootstrap modal popup here
         } else {
             echo '<script language="javascript">';
             echo 'alert("' . $errorMsg . '")';
